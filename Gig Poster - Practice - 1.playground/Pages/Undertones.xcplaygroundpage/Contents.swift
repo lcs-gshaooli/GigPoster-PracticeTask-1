a767ba64692b458c520ad1b3ed44e4ade6cbe4b4
Gabriela Shaooli <gshaooli@lcs.on.ca>
2020-01-30 11:14:42 -0500

    Make background and add text of the poster.

--- Gig Poster - Practice - 1.playground/Pages/Undertones.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 1.playground/Pages/Undertones.xcplaygroundpage/Contents.swift
@@ -29,40 +29,67 @@ let canvas = Canvas(width: 400, height: 600)
 let lightGrey = Color(hue: 84, saturation: 6, brightness: 88, alpha: 100)
 let deepRed = Color(hue: 7, saturation: 97, brightness: 72, alpha: 100)
 
+//// Let's see where the origin is
+//canvas.drawAxes()
+//
+//// Move the origin 100 pixels right, 200 pixels up
+//canvas.translate(to:Point(x: 100, y: 200))
+//
+//// Let's see where the origin is
+//canvas.drawAxes()
+//
+//// Draw a square at the origin
+//canvas.drawRectangle(at: Point(x: 0, y: 0), width: 50, height: 50)
+//
+//// Make shapes have no fill
+//canvas.drawShapesWithFill = false
+//
+//// Move the origin again - over 100, up 300
+//canvas.translate(to: Point(x:100, y:300))
+//
+////Check to see wherte origin is
+//canvas.drawAxes()
+//
+//// Make a loop, draw six squares rotated around current origin
+//for _ in 1...6 {
+//    // Rotate origin
+//    canvas.rotate(by: 60) // 360/ 6 = 600
+//
+//    // Draw axes
+//   //canvas.drawAxes()
+//
+//    // Draw square
+//    canvas.drawRectangle(at: Point(x:0, y:0), width: 50, height: 50)
+//
+//}
+
 // Begin your solution here...
-// Let's see where the origin is
 canvas.drawAxes()
 
-// Move the origin 100 pixels right, 200 pixels up
-canvas.translate(to:Point(x: 100, y: 200))
+// Background
+canvas.fillColor = lightGrey
+canvas.drawEllipse(at: Point(x:200, y:300), width: 600, height: 800)
 
-// Let's see where the origin is
-canvas.drawAxes()
+// Text color
+canvas.textColor = deepRed
 
-// Draw a square at the origin
-canvas.drawRectangle(at: Point(x: 0, y: 0), width: 50, height: 50)
+// Text
+canvas.drawText(message: "straight music present", at: Point (x:25, y:550), size: 10)
+canvas.drawText(message: "the undertones", at: Point (x:25, y:540), size: 10)
 
-// Make shapes have no fill
-canvas.drawShapesWithFill = false
+canvas.drawText(message: "with special guest", at: Point (x:25, y:520), size: 10)
+canvas.drawText(message: "the chords", at: Point (x:25, y:510), size: 10)
 
-// Move the origin again - over 100, up 300
-canvas.translate(to: Point(x:100, y:300))
+canvas.drawText(message: "victoria hall hanley", at: Point (x:150, y:550), size: 10)
+canvas.drawText(message: "stoke-on-trent, england", at: Point (x:150, y:540), size: 10)
 
-//Check to see wherte origin is
-canvas.drawAxes()
+canvas.drawText(message: "tuesday", at: Point (x:150, y:520), size: 10)
+canvas.drawText(message: "june 5 1979 / 7:30 pm", at: Point (x:150, y:510), size: 10)
 
-// Make a loop, draw six squares rotated around current origin
-for _ in 1...6 {
-    // Rotate origin
-    canvas.rotate(by: 60) // 360/ 6 = 600
-    
-    // Draw axes
-   //canvas.drawAxes()
-    
-    // Draw square
-    canvas.drawRectangle(at: Point(x:0, y:0), width: 50, height: 50)
-    
-}
+canvas.drawText(message: "advance tickets: £2.00", at: Point (x:275, y:550), size: 10)
+canvas.drawText(message: "at the door:£2.50", at: Point (x:275, y:540), size: 10)
+
+
 /*:
  ## Use Source Control
  
